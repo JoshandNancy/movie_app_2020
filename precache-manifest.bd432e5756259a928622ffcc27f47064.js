70c748174a08a448e41c61a6e22e5b21d1f03735
Junweon Park <merlin@jellybus.com>
2020-04-11 19:51:52 +0900

    Updates

---
+++ precache-manifest.bd432e5756259a928622ffcc27f47064.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6884417d705d60833742b5584ac983b",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1747ec5218af1d35d19a",
+    "url": "/movie_app_2020/static/css/main.2180cebe.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1747ec5218af1d35d19a",
+    "url": "/movie_app_2020/static/js/main.a9d55c54.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
